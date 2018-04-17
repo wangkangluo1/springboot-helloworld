2a612734532de6651270e6b1683087c428f864dd
康乐 王 <wangkangluo1@gmail.com>
2018-04-17 13:31:34 +0800

    update

--- src/main/java/org/roy/helloworld/HelloworldApplication.java
+++ src/main/java/org/roy/helloworld/HelloworldApplication.java
@@ -1,12 +1,28 @@
 package org.roy.helloworld;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@RestController
+	@EnableAutoConfiguration
+	public class FileUploadController {
+
+		@RequestMapping("/test")
+		void test(HttpServletRequest request){
+			System.out.println(request.getRemoteAddr());
+		}
+
 	}
 }
